2e76383cf2e363aed1577fee1451b605b0ecf663
SebastianGtr <tanoriruizs@gmail.com>
2022-04-09 12:59:22 -0700

    Agregacion de Clase Lista

---
+++ ListasSimplementeLigadas/Nodo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasSimplementeLigadas
+{
+    public class Nodo
+    {
+        public string Valor { get; set; }
+
+        public Nodo Enlace { get; set; }
+
+        public Nodo(string valor="", Nodo enlace=null)
+        {
+            Valor = valor;
+            Enlace = enlace;
+           
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
