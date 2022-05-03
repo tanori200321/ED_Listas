aca03b13478cb529660d9f71123bb1a1afeac1af
SebastianGtr <tanoriruizs@gmail.com>
2022-05-03 11:55:24 -0700

    Nodo

---
+++ ListaDobleMenteLigadasCirculares/Nodo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDobleMenteLigadasCirculares
+{
+    internal class Nodo
+    {
+        public string Valor { get; set; }
+        public Nodo EnlaceSiguiente { get; set; }
+        public Nodo EnlaceAnterior{ get; set; }
+
+
+        public Nodo(string valor = "", Nodo enlaceSiguiente = null, Nodo enlaceAnterior = null)
+        {
+            Valor = valor;
+            EnlaceSiguiente = enlaceSiguiente;
+            enlaceAnterior = enlaceAnterior;
+        }
+    }
+}
---
+++ ListaDobleMenteLigadasCirculares/Program.cs
@@ -0,0 +1,21 @@
+﻿using ListaDobleMenteLigadasCirculares;
+
+Lista listas = new Lista();
+listas.AgregarNodo("Cero");
+listas.AgregarNodo("Uno");
+listas.AgregarNodo("Dos");
+listas.AgregarNodo("Tres");
+Console.WriteLine(listas.RecorrerLista());
+
+Console.WriteLine("-------Buscando Uno---------");
+Nodo nodoBusqueda = listas.Buscar("Uno");
+Console.WriteLine(nodoBusqueda.Valor);
+
+
+Console.WriteLine("-------Buscando Tres--------");
+Nodo nodoDos = listas.Buscar("Tres");
+Console.WriteLine(nodoDos.Valor);
+
+Console.WriteLine("-------Borrando Tres--------");
+listas.BorrarNodo("Tres");
+Console.WriteLine(listas.RecorrerLista());
